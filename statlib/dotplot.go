1fc55a9a93832dbf44656f7ecb365fa229f11f54
Alexander Campbell <alexanderhcampbell@gmail.com>
2014-11-01 00:42:42 -0500

    Implement Dataset.Dotplot with unit test

---
+++ statlib/dotplot.go
@@ -0,0 +1,43 @@
+package statlib
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Dotplot will make an attempt to fit within the specified `width` in columns.
+func (d *Dataset) Dotplot(width int) string {
+	bucketCount := width / 6 // we need about 6 columns per dotplot
+	buckets := d.Buckets(bucketCount)
+	bucketSize := d.Range() / float64(bucketCount)
+
+	buf := new(bytes.Buffer)
+
+	maxHeight := 0
+	bottomLine := ``
+	for i, bucket := range buckets {
+		// TODO: need a good way to force this string to be below six
+		// characters without truncating and ending up with a result
+		// that looks like the right number but could be off by an order
+		// of magnitude
+		s := fmt.Sprintf("%5.2f ", d.records[0]+(bucketSize*float64(i)))
+		bottomLine = bottomLine + s
+		if bucket != nil && bucket.len > maxHeight {
+			maxHeight = bucket.len
+		}
+	}
+
+	for i := 0; i < maxHeight; i++ {
+		line := ``
+		for _, bucket := range buckets {
+			if bucket != nil && bucket.len >= maxHeight-i {
+				line = line + `  .   `
+			} else {
+				line = line + `      `
+			}
+		}
+		fmt.Fprintf(buf, "%s\n", line)
+	}
+	fmt.Fprintf(buf, "%s\n", bottomLine)
+	return buf.String()
+}
---
+++ statlib/dotplot_test.go
@@ -0,0 +1,16 @@
+package statlib
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDataset_Dotplot(t *testing.T) {
+	d := NewDataset(1, 2, 3, 4, 4, 4, 7, 8)
+	assert.Equal(t, "  .     .         \n"+
+		"  .     .     .   \n"+
+		"  .     .     .   \n"+
+		" 1.00  3.33  5.67 \n",
+		d.Dotplot(20))
+}
