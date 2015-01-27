128a88fe8dc8f9645bc8515a5131742d94500227
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-01-26 19:41:21 -0600

    Update Histogram test

--- statlib/histogram_test.go
+++ statlib/histogram_test.go
@@ -8,9 +8,10 @@ import (
 
 func TestDataset_Histogram(t *testing.T) {
 	d := NewDataset(1, 2, 3, 4, 4, 4, 7, 8)
-	assert.Equal(t, "  *     *         \n"+
-		"  *     *     *   \n"+
-		"  *     *     *   \n"+
-		" 1.00  3.33  5.67 ",
-		d.Histogram(20))
+	assert.Equal(t, ""+
+		"   *       *            \n"+
+		"   *       *       *    \n"+
+		"   *       *       *    \n"+
+		"   1.00    3.33    5.67 ",
+		d.Histogram(30))
 }
